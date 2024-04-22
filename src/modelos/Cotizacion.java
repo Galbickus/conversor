093074d9672014f94dd9d12b481638dbc968636f
Veronica <vgalbicka@gmail.com>
2024-04-22 11:45:23 -0300

    add  Class Cotizacion.java

---
+++ src/modelos/Cotizacion.java
@@ -0,0 +1,28 @@
+package modelos;
+
+public class Cotizacion {
+    private String bob;
+    private String ars;
+    private String usd;
+    private String brl;
+    private String col;
+    private String cny;
+    private String egp;
+    private String aud;
+    private String pen;
+    private String mxn;
+
+    public Cotizacion(String bob, String ars, String usd, String brl, String col, String cny, String egp, String aud, String pen, String mxn) {
+        this.bob = bob;
+        this.ars = ars;
+        this.usd = usd;
+        this.brl = brl;
+        this.col = col;
+        this.cny = cny;
+        this.egp = egp;
+        this.aud = aud;
+        this.pen = pen;
+        this.mxn = mxn;
+    }
+}
+
