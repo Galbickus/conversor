b4c4fe01b99590a1ebb86849eec77522aaeba70e
Veronica <vgalbicka@gmail.com>
2024-04-22 11:28:07 -0300

    record CotizacionOmdb

---
+++ src/modelos/CotizacionOmdb.java
@@ -0,0 +1,6 @@
+package modelos;
+
+public record CotizacionOmdb(String usd, String bob, String ars, String brl, String col, String cny,
+                             String egp, String aud, String pen, String mxn) {
+}
+
