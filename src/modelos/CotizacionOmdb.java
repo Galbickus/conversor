01a6f1f844a2b9e2079882400b1bff1a77171062
Veronica <vgalbicka@gmail.com>
2024-04-22 11:47:02 -0300

    change

--- src/modelos/CotizacionOmdb.java
+++ src/modelos/CotizacionOmdb.java
@@ -1,6 +1,14 @@
 package modelos;
 
-public record CotizacionOmdb(String usd, String bob, String ars, String brl, String col, String cny,
-                             String egp, String aud, String pen, String mxn) {
+public record CotizacionOmdb(String usd,
+                             String bob,
+                             String ars,
+                             String brl,
+                             String col,
+                             String cny,
+                             String egp,
+                             String aud,
+                             String pen,
+                             String mxn) {
 }
 
